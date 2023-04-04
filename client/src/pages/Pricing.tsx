cc1f74917c00ae1e9dba4e671db44b24f6d72db8
patrikzita <patrikzit@seznam.cz>
2023-04-04 20:22:17 +0200

    The basic structure of PricingCard

--- client/src/pages/Pricing.tsx
+++ client/src/pages/Pricing.tsx
@@ -1,39 +1,53 @@
 import { SubHeader } from "../component/Header/SubHeader";
 
-
 const PRICING_BOXES = [
-    {
-        level: "Free",
-        price: "0.00",
-        benefits: ["Educative material", "1 lesson", "Access to community forum"]
+  {
+    level: "Free",
+    price: "0.00",
+    benefits: ["Educative material", "1 lesson", "Access to community forum"],
+  },
+  {
+    level: "Pro",
+    price: "18.99",
+    benefits: [
+      "Unlimited lessons",
+      "Advanced techniques",
+      "Exclusive training videos",
+      "Access to live webinars",
+      "Priority email support",
+      "10% discount on merchandise",
+    ],
+  },
+  {
+    level: "Premium Pro",
+    price: "24.99",
+    benefits: [
+      "All Pro benefits",
+      "Personalized training plan",
+      "1-on-1 coaching session per month",
+      "Exclusive access to premium content",
+      "Priority access to special events",
+      "15% discount on merchandise",
+    ],
+  },
+];
 
-    },
-    {
-        level: "Pro",
-        price: "18.99",
-        benefits: [
-            "Unlimited lessons",
-            "Advanced techniques",
-            "Exclusive training videos",
-            "Access to live webinars",
-            "Priority email support",
-            "10% discount on merchandise"
-        ]
-
-    },
-    {
-        level: "Premium Pro",
-        price: "24.99",
-        benefits: [
-            "All Pro benefits",
-            "Personalized training plan",
-            "1-on-1 coaching session per month",
-            "Exclusive access to premium content",
-            "Priority access to special events",
-            "15% discount on merchandise"
-        ]
-    },
-]
+type PricingCardProsp = {
+  level: string;
+  price: string;
+  benefits: string[];
+};
+const PricingCard = ({ level, price, benefits }: PricingCardProsp) => {
+  return (
+    <div className="flex flex-col p-5 border-2">
+      <h1 className="text-2xl font-bold">{level}</h1>
+      <h2 className="text-3xl font-bold">${price}</h2>
+      {benefits.map((benefit) => (
+        <p>{benefit}</p>
+      ))}
+    </div>
+  );
+};
 
 /* TODO: Dokončit tuto sekci */
 
@@ -48,6 +62,11 @@ export const Pricing = () => {
             Gymat an unknown printer took a galley of type and scrambled make a
             type specimen book.
           </p>
+          <div className="flex flex-col">
+            {PRICING_BOXES.map((item) => (
+              <PricingCard key={item.level} {...item} />
+            ))}
+          </div>
         </div>
       </div>
     </main>
