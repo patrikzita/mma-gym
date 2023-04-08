12f8ff528088033f58ea5fe961f3472ae1bd79e8
patrikzita <patrikzit@seznam.cz>
2023-04-08 15:03:42 +0200

    merch photos added to project

--- client/src/pages/Merch.tsx
+++ client/src/pages/Merch.tsx
@@ -6,7 +6,7 @@ const PRODUCTS = [
   {
     id: "gloves001",
     name: "Boxing Gloves",
-    img: "images/signup-image.png",
+    img: "images/products/small/boxing_gloves.png",
     price: 39.99,
     quantity: 10,
     category: "boxing",
@@ -14,7 +14,7 @@ const PRODUCTS = [
   {
     id: "shinguards001",
     name: "Shin Guards",
-    img: "images/signup-image.png",
+    img: "images/products/small/shin_guards.png",
     price: 29.99,
     quantity: 5,
     category: "MMA",
@@ -22,7 +22,7 @@ const PRODUCTS = [
   {
     id: "mouthguard001",
     name: "Mouthguard",
-    img: "images/signup-image.png",
+    img: "images/products/small/mouthguard.png",
     price: 9.99,
     quantity: 20,
     category: "boxing",
@@ -30,7 +30,7 @@ const PRODUCTS = [
   {
     id: "headgear001",
     name: "Boxing Headgear",
-    img: "images/signup-image.png",
+    img: "images/products/small/boxing_headgear.png",
     price: 49.99,
     quantity: 2,
     category: "boxing",
@@ -38,7 +38,7 @@ const PRODUCTS = [
   {
     id: "focusmitts001",
     name: "Focus Mitts",
-    img: "images/signup-image.png",
+    img: "images/products/small/focus_mitts.png",
     price: 29.99,
     quantity: 8,
     category: "boxing",
@@ -46,7 +46,7 @@ const PRODUCTS = [
   {
     id: "punchingbag001",
     name: "Punching Bag",
-    img: "images/signup-image.png",
+    img: "images/products/small/punching_bag.png",
     price: 149.99,
     quantity: 1,
     category: "boxing",
@@ -54,7 +54,7 @@ const PRODUCTS = [
   {
     id: "skippingrope001",
     name: "Skipping Rope",
-    img: "images/signup-image.png",
+    img: "images/products/small/skipping_rope.png",
     price: 14.99,
     quantity: 15,
     category: "fitness",
@@ -62,7 +62,7 @@ const PRODUCTS = [
   {
     id: "weightedvest001",
     name: "Weighted Vest",
-    img: "images/signup-image.png",
+    img: "images/products/small/weighted_vest.png",
     price: 69.99,
     quantity: 3,
     category: "fitness",
@@ -70,7 +70,7 @@ const PRODUCTS = [
   {
     id: "resistancebands001",
     name: "Resistance Bands",
-    img: "images/signup-image.png",
+    img: "images/products/small/resistance_bands.png",
     price: 19.99,
     quantity: 10,
     category: "fitness",
@@ -78,7 +78,7 @@ const PRODUCTS = [
   {
     id: "grapplingdummy001",
     name: "Grappling Dummy",
-    img: "images/signup-image.png",
+    img: "images/products/small/grappling_dummy.png",
     price: 199.99,
     quantity: 1,
     category: "MMA",
@@ -94,17 +94,16 @@ type ProductCardProps = {
 
 const ProductCard = ({ id, name, img, price }: ProductCardProps) => {
   return (
-    <div className="flex flex-col w-fit border-2 p-5 items-center gap-y-8 mx-auto">
-      <img src={img} alt="" className="w-64 cursor-pointer" />
+    <div className="flex flex-col w-fit p-5 items-center gap-y-8 mx-auto hover:border-2">
+      <img src={img} alt="" className="w-max-64 cursor-pointer" />
       <h1 className="text-md text-blue-400 font-medium cursor-pointer hover:text-blue-300">
-        {/* TODO: Zjistit jak opravit hover */}
         {name}
       </h1>
       <div className="flex flex-col text-center gap-y-2">
         <h2 className="text-xl text-red-500 font-medium">
           {formatCurrency(price, "USD")}
         </h2>
-        <button className="flex items-center gap-x-2 px-2 py-1 hover:drop-shadow-md">
+        <button className="flex items-center border-2 border-gray-100 gap-x-2 px-2 py-1 hover:drop-shadow-sm">
           <MdShoppingCart />
           Add to Cart
         </button>
@@ -117,7 +116,7 @@ export const Merch = () => {
   return (
     <main>
       <SubHeader title="Merch" />
-      <div className="grid grid-cols-1 border-2 md:grid-cols-3 lg:grid-cols-4 gap-5 mx-5 my-32 just">
+      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-5 mx-5 my-32 just">
         {PRODUCTS.map((product) => (
           <ProductCard
             key={product.id}
