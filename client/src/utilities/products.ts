86d7c0cc14fc54c1170616a94e88991e0c6c243b
Patrik Zita <patrikzit@seznam.cz>
2023-05-04 08:24:12 +0200

    Add API server

--- client/src/utilities/products.ts
+++ client/src/utilities/products.ts
@@ -1,12 +1,14 @@
 import axios from "axios";
 
+const API_URL = import.meta.env.VITE_APP_API_URL;
+
 export async function getProductsDataFetch() {
-  return axios.get("http://localhost:3000/products").then((res) => res.data);
+  return axios.get(`${API_URL}/products`).then((res) => res.data);
 }
 
 export async function getProductDataFetch(id: string) {
   return axios
-    .get(`http://localhost:3000/products/${id}`)
+    .get(`${API_URL}/products/${id}`)
     .then((res) => res.data)
     .catch((error) => {
       console.error(error);
--- client/src/utilities/trainers.ts
+++ client/src/utilities/trainers.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+const API_URL = import.meta.env.VITE_APP_API_URL;
+
+
 export async function getTrainersDataFetch(count?: number) {
-  return axios.get("http://localhost:3000/trainers").then((res) => {
+  return axios.get(`${API_URL}/trainers`).then((res) => {
     const trainers = res.data;
     return count ? trainers.slice(0, count) : trainers;
   });
@@ -9,7 +12,7 @@ export async function getTrainersDataFetch(count?: number) {
 
 export async function getTrainerDataFetch(id: string) {
   return axios
-    .get(`http://localhost:3000/trainers/${id}`)
+    .get(`${API_URL}/trainers/${id}`)
     .then((res) => res.data)
     .catch((error) => {
       console.error(error);
